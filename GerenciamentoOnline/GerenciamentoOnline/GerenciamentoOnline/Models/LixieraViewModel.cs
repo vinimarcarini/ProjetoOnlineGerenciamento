a5055daff2863e53349ec6ceb5babdbbf3862225
vinimarcarini <csmala@hotmail.com>
2017-12-15 23:50:28 -0200

    Commit Inicial
    Commit Inicial

---
+++ GerenciamentoOnline/GerenciamentoOnline/GerenciamentoOnline/Models/LixieraViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GerenciamentoOnline.Models
+{
+    public class LixieraViewModel
+    {
+
+        [Key]
+        public int ProdutoId { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Nome")]
+        [MaxLength(250, ErrorMessage = "Máximo {0} caracteres")]
+        [MinLength(2, ErrorMessage = "Mínimo {0} caracteres")]
+        public string Nome { get; set; }
+
+
+        [DataType(DataType.Currency)]
+        [Range(typeof(decimal), "0", "999999999999")]
+        [Required(ErrorMessage = "Preencha um valor")]
+        public decimal Valor { get; set; }
+
+        [DisplayName("Disponivel?")]
+        public bool Disponivel { get; set; }
+        public int ClienteId { get; set; }
+
+      
+}
